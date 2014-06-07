8f03a24cbd9d21f133f458196619498878b292a5
mytory <mytory@gmail.com>
2014-06-07 13:00:47 +0900

    워드프레스 종속성 제거

--- postcode-search/postcode-search.php
+++ postcode-search/postcode-search.php
@@ -1,4 +1,6 @@
 <?php
+
+if(function_exists('plugins_url')){
     class Postcode_Search {
         static function get_endpoint () {
             return plugins_url('search.php', __FILE__);
@@ -11,22 +13,23 @@
 
     wp_localize_script('postcode-search', 'postcode_search',
                        array('ajaxurl' => Postcode_Search::get_endpoint()));
+}
 
-    function postcode_search_form ($opts=array()) {
-        $opts = array_merge(array(
-            'id' => 'postcode-search', 
-            'classes'     => '',
-            'head-name'   => 'postcode[]',
-            'head-value'  => '',
-            'tail-name'   => 'postcode[]',
-            'tail-value'  => '',
-            'addr-name'   => 'address',
-            'addr-value'  => '',
-            'addr-size'   => 20,
-            'spinner-img' => plugins_url('img/spinner.gif', __FILE__),
-            'search-text' => '우편번호 찾기',
-            'prev-classes' => '',
-            'next-classes' => '',
-        ), $opts);
-        include plugin_dir_path(__FILE__) . '/search-form.php';
-    }
+function postcode_search_form ($opts=array()) {
+    $opts = array_merge(array(
+        'id' => 'postcode-search',
+        'classes'     => '',
+        'head-name'   => 'postcode[]',
+        'head-value'  => '',
+        'tail-name'   => 'postcode[]',
+        'tail-value'  => '',
+        'addr-name'   => 'address',
+        'addr-value'  => '',
+        'addr-size'   => 20,
+        'spinner-img' => plugins_url('img/spinner.gif', __FILE__),
+        'search-text' => '우편번호 찾기',
+        'prev-classes' => '',
+        'next-classes' => '',
+    ), $opts);
+    include plugin_dir_path(__FILE__) . '/search-form.php';
+}
